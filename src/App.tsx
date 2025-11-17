042ab7dba754476d476d061ce5036f73d2484dad
osamadweikat <osamadweikat83@gmail.com>
2025-11-17 18:57:24 +0200

    Create Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Home from "./pages/home/home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,32 @@
+import "./navbar.css";
+import BrandLogo from "../../images/brand-logo.png";
+import MenuIcon from "../../images/menu-icon.svg";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar-container">
+      <div className="barnd-conatiner">
+        <img src={BrandLogo} alt="brand-logo" className="brand-logo" />
+        <h3 className="brand-text">Yummy</h3>
+      </div>
+      <div className="nav-menu">
+        <ul className="navbar-links">
+          <li>home</li>
+          <li>book a table</li>
+          <li>gallery</li>
+          <li>house dishes</li>
+          <li>events</li>
+          <li>contact</li>
+        </ul>
+      </div>
+      <div className="navbar-button-wrapper">
+        <button className="navbar-button">
+          <img src={MenuIcon} alt="menu-icon" />
+          yummy menu
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
