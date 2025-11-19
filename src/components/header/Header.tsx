12374254e92bdf4686ba99026a14da1d641c1026
osamadweikat <osamadweikat83@gmail.com>
2025-11-19 23:22:34 +0200

    feat: Make Facts component fully responsive

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -14,80 +14,82 @@ const Header = () => {
   const { setRef, inViewIndexes } = useInView<HTMLElement>();
 
   return (
-    <header className="header-section">
+    <>
       <Navbar />
-      <div className="header-container">
-        <div className="header-contact-container">
-          <button>
-            <img src={PhoneWhite} alt="phone" />
-          </button>
-          <button>
-            <img src={MailWhite} alt="mail" />
-          </button>
-          <button>
-            <img src={PinWhite} alt="pin" />
-          </button>
-        </div>
-        <div className="header-social-container">
-          <button>
-            <img src={FacebookLogo} alt="facebook" />
-          </button>
-          <button>
-            <img src={TwitterLogo} alt="twitter" />
-          </button>
-          <button>
-            <img src={InstagramLogo} alt="instagram" />
-          </button>
-        </div>
-        <div className="header-text-container">
-          <div className="header-vertical-divider" />
-          <div className="header-photo-container">
-            <h1
-              ref={setRef}
-              className={`header-title1 ${
-                inViewIndexes.includes(0) ? "animate-left" : ""
-              }`}
-            >
-              yummy
-            </h1>
+      <header className="header-section">
+        <div className="header-container">
+          <div className="header-contact-container">
+            <button>
+              <img src={PhoneWhite} alt="phone" />
+            </button>
+            <button>
+              <img src={MailWhite} alt="mail" />
+            </button>
+            <button>
+              <img src={PinWhite} alt="pin" />
+            </button>
+          </div>
+          <div className="header-social-container">
+            <button>
+              <img src={FacebookLogo} alt="facebook" />
+            </button>
+            <button>
+              <img src={TwitterLogo} alt="twitter" />
+            </button>
+            <button>
+              <img src={InstagramLogo} alt="instagram" />
+            </button>
+          </div>
+          <div className="header-text-container">
+            <div className="header-vertical-divider" />
+            <div className="header-photo-container">
+              <h1
+                ref={setRef}
+                className={`header-title1 ${
+                  inViewIndexes.includes(0) ? "animate-left" : ""
+                }`}
+              >
+                yummy
+              </h1>
 
-            <h1
-              ref={setRef}
-              className={`header-title2 ${
-                inViewIndexes.includes(1) ? "animate-right" : ""
-              }`}
-            >
-              tummy
-            </h1>
+              <h1
+                ref={setRef}
+                className={`header-title2 ${
+                  inViewIndexes.includes(1) ? "animate-right" : ""
+                }`}
+              >
+                tummy
+              </h1>
 
-            <div className="header-icon-container" />
-            <div className="header-secondary-image">
-              <img src={HeaderBg2} alt="" />
+              <div className="header-icon-container" />
+              <div className="header-secondary-image">
+                <img src={HeaderBg2} alt="" />
+              </div>
+            </div>
+            <div className="header-vertical-divider" />
+          </div>
+          <h6 className="opening-hours-text">
+            <span>opening hours: </span>mon-sun, 8am - 10pm{" "}
+          </h6>
+          <h6 className="header-phone-text">
+            make a reservation: <span>+1 917-695-4445</span>
+          </h6>
+        </div>
+        <div className="header-nav-menu-wrapper">
+          <div className="color-bullet" />
+          <div className="header-menu-list-wrapper">
+            <div className="header-menu-list">
+              {miniNavbarLinks.slice(2).map((link) => (
+                <div key={link.id} className="header-menu-item">
+                  <button>{link.label}</button>
+                  <div className="color-bullet" />
+                </div>
+              ))}
             </div>
           </div>
-          <div className="header-vertical-divider" />
         </div>
-        <h6 className="opening-hours-text">
-          <span>opening hours: </span>mon-sun, 8am - 10pm{" "}
-        </h6>
-        <h6 className="header-phone-text">
-          make a reservation: <span>+1 917-695-4445</span>
-        </h6>
-      </div>
-      <div className="header-nav-menu-wrapper">
-        <div className="color-bullet" />
-        <div className="header-menu-list-wrapper">
-          <div className="header-menu-list">
-            {miniNavbarLinks.slice(2).map((link) => (
-              <div key={link.id} className="header-menu-item">
-                <button>{link.label}</button>
-                <div className="color-bullet" />
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 };
 
