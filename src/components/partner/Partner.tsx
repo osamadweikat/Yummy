8728f31a8059efed0d119dbd0d7234b24ec9cbf4
osamadweikat <osamadweikat83@gmail.com>
2025-11-21 12:38:44 +0200

    style: add styling for Partner component

--- src/components/partner/Partner.tsx
+++ src/components/partner/Partner.tsx
@@ -3,7 +3,7 @@ import { Partners } from "../../types/partners.types";
 
 const Partner = () => {
   return (
-    <section className="partner-section">
+    <section className="partners-section">
       {Partners.map((partner) => (
         <div key={partner.id} className="partner-logo-wrapper">
           <img src={partner.logo} alt="partner-logo" />
