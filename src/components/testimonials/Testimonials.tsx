b0bfadc1f19c22928a4f3bd890bba4a62062e0dc
osamadweikat <osamadweikat83@gmail.com>
2025-11-21 15:27:15 +0200

    Add scroll-triggered animation to Testimonials component

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,15 +1,24 @@
 import "./testimonials.css";
 import QuoteImage from "../../images/quote.svg";
 import { testimonials } from "../../types/testimonials.types";
+import { useInView } from "../../hooks/useInView";
 
 const Testimonials = () => {
+  const { setRef, inViewIndexes } = useInView<HTMLDivElement>();
+
   return (
     <section className="testimonials-section">
       <div className="testimonials-container">
-        {testimonials.map((testimonial) => {
+        {testimonials.map((testimonial, index) => {
           const [firstName, lastName] = testimonial.name.split(" ");
+          const isInView = inViewIndexes.includes(index);
+
           return (
-            <div key={testimonial.id} className="testimonials-wrapper">
+            <div
+              key={testimonial.id}
+              ref={setRef}
+              className={`testimonials-wrapper ${isInView ? "in-view" : ""}`}
+            >
               <img src={testimonial.image} alt="" className="quote-photo" />
               <img src={QuoteImage} alt="" className="quote-sign" />
               <p className="testimonials-quote">{testimonial.quote}</p>
